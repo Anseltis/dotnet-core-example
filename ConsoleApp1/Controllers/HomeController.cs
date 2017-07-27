7d01c68ca58df5cb96c13adbd3950cb3374e5f07
Anseltis <the-ansi@yandex.ru>
2017-07-27 23:27:01 +0300

    Add project files.

---
+++ ConsoleApp1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApp1.Controllers
+{
+    public class HomeController: Controller
+    {
+        [Route("index"), HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            return Json(new { values = 100 });
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Running demo with Kestrel.");
+
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var builder = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    if (config["threadCount"] != null)
+                    {
+                        options.ThreadCount = int.Parse(config["threadCount"]);
+                    }
+                })
+                .UseUrls("http://localhost:5000");
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
